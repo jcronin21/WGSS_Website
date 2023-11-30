ac14679a0fc978b0d43408eb4edcfe72a2a43fff
Jazz <jcronin@unca.edu>
2023-11-30 01:35:44 -0500

    catching up

--- scripts.js
+++ scripts.js
@@ -6,3 +6,35 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+// Add this script to your existing JavaScript or create a new script file
+
+// Function to display comments from local storage
+function displayComments() {
+    const commentsContainer = document.getElementById('comments-list');
+    const comments = JSON.parse(localStorage.getItem('comments')) || [];
+  
+    commentsContainer.innerHTML = '';
+    comments.forEach(comment => {
+      const commentDiv = document.createElement('div');
+      commentDiv.textContent = comment;
+      commentsContainer.appendChild(commentDiv);
+    });
+  }
+  
+  // Event listener for form submission
+  const commentForm = document.getElementById('comment-form');
+  commentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+    const commentContent = document.getElementById('comment').value;
+    const comments = JSON.parse(localStorage.getItem('comments')) || [];
+    
+    comments.push(commentContent);
+    localStorage.setItem('comments', JSON.stringify(comments));
+  
+    displayComments();
+  });
+  
+  // Display comments on page load
+  window.addEventListener('load', displayComments);
+  
